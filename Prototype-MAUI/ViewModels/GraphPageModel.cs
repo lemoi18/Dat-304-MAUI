19b39cfb096d1c465b4ed4b4db123aaafac2a6dd
giarr <giarreh2@gmail.com>
2023-04-03 13:33:42 +0200

    Removed LiveCharts 2 from project, going to try out SyncFusion instead

--- Prototype-MAUI/ViewModels/GraphPageModel.cs
+++ Prototype-MAUI/ViewModels/GraphPageModel.cs
@@ -4,22 +4,12 @@ using MauiApp8.Model;
 using CommunityToolkit.Mvvm.ComponentModel;
 using MauiApp8.Services.Authentication;
 using MauiApp8.Services.BackgroundServices;
-using LiveChartsCore;
-using LiveChartsCore.SkiaSharpView;
 
 
 namespace MauiApp8.ViewModel
 {
     public partial class GraphPageModel : ObservableObject
     {
-    public <ISeries[] Series { get; set; }
-        = new ISeries[]
-        {
-        new LineSeries<double>
-            {
-            Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
-            Fill = null
-            }
-        };
+
     }
 }
