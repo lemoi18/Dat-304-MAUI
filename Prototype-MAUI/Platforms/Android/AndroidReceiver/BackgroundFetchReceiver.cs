36e2571245947392747e5cbac4548366de44216a
Oskar Eftedal Markussen <oskar@uia.no>
2023-04-10 15:24:50 +0200

    added pulish subscribe to background fetch and automatic start of background fetch with foreground fetch

--- Prototype-MAUI/Platforms/Android/AndroidReceiver/BackgroundFetchReceiver.cs
+++ Prototype-MAUI/Platforms/Android/AndroidReceiver/BackgroundFetchReceiver.cs
@@ -4,20 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Android.Content;
+using PS = MauiApp8.Services.PublishSubscribeService;
 using MauiApp8.Services.BackgroundServices;
+using IBF = MauiApp8.Services.BackgroundServices;
 
 namespace MauiApp8.Platforms.Android.AndroidReceiver
 {
     [BroadcastReceiver(Enabled = true)]
     public class BackgroundFetchReceiver : BroadcastReceiver
     {
+
+       
+
         
         public override async void OnReceive(Context context, Intent intent)
         {
             // Implement your background fetch logic here
-            DataBase db = new DataBase();
-            await db.UpdateGlucose("https://oskarnightscoutweb1.azurewebsites.net/");
-            await db.UpdateInsulin("https://oskarnightscoutweb1.azurewebsites.net/");
+            IBF.IBackgroundService ibf = new IBF.DataBase();
+            PS.Publish _publish = new PS.Publish(ibf);
+            await _publish.UpdateBackgroundData("https://oskarnightscoutweb1.azurewebsites.net/");
+            //DataBase db = new DataBase();
+            //await db.UpdateGlucose("https://oskarnightscoutweb1.azurewebsites.net/");
+            //await db.UpdateInsulin("https://oskarnightscoutweb1.azurewebsites.net/");
             Console.WriteLine("Testing Background fetch...");
         }
     }
--- Prototype-MAUI/Services/BackgroundServices/DB-NightscoutBridge.cs
+++ Prototype-MAUI/Services/BackgroundServices/DB-NightscoutBridge.cs
@@ -183,7 +183,8 @@ namespace MauiApp8.Services.BackgroundServices
             Console.WriteLine("Adding " + Items.Count + " entries... ");
             foreach (GlucoseAPI obj in Items)
             {
-                DB.AddGlucoseEntry(obj.sgv, obj.dateString);
+                if (obj.sgv != null)
+                    DB.AddGlucoseEntry(obj.sgv, obj.dateString);
             }
             return 200;
         }
