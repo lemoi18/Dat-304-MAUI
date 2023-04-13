85d56b0bec5b4c65fc2e72b28311a3140265589c
Ole Markus Kvisgaard Roland <omroland@student.uia.no>
2023-04-13 14:46:06 +0200

    Added no internet connection warning

---
+++ Prototype-MAUI/Controls/NoInternetWarning.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiApp8.Controls
+{
+    public partial class NoInternetWarning : ContentView
+    {
+        public NoInternetWarning()
+        {
+            InitializeComponent();
+
+            Connectivity.ConnectivityChanged += OnConnectivityChanged;
+            CheckConnectivity();
+        }
+
+        private void OnConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            CheckConnectivity();
+        }
+
+        private void CheckConnectivity()
+        {
+            bool isConnected = Connectivity.NetworkAccess == NetworkAccess.Internet;
+            NoInternetWarningLabel.IsVisible = !isConnected;
+            NoInternetWarningLabel.HeightRequest = isConnected ? 0 : 30; // Set the height to 30 when not connected
+        }
+
+    }
+}
